b8f6de1b1b82248e61e2c5f3952b467c6b6a58fe
Bruno Robles <roblesbruno76@gmail.com>
2023-11-14 18:46:07 -0300

    ENTREGA FIUAL

--- carrito.js
+++ carrito.js
@@ -4,43 +4,6 @@
 
 // ESTE ARRAY CONTIENE LOS PRODUCTOS REMERAS QUE SE OFRECESN EN EL LOCAL 
 
-const remerasCustom = [
-    {
-        id: "001",
-        nombre: "MOTORCYCLE ",
-        talle: "S  M  L  XL",
-        precio: 12500,
-        img: "./remeras/motorcycle-back.jpg",
-        cantidad: 1
-    },
-
-    {
-        id: "002",
-        nombre: "OILY",
-        talle: "S  M  L  XL",
-        precio: 11500,
-        img: "./remeras/OILY.jpg",
-        cantidad: 1
-    },
-
-    {
-        id: "003",
-        nombre: "OR COMPANY",
-        talle: "S  M  L  XL",
-        precio: 13500,
-        img: "./remeras/OR COMPANY.jpg",
-        cantidad: 1
-    },
-
-    {
-        id: "004",
-        nombre: "SHAPPER NAR",
-        talle: "S  M  L  XL",
-        precio: 12800,
-        img: "./remeras/shapper-nar.jpg",
-        cantidad: 1
-    },
-]
 
 
 //CAPTURO EL CONTENEDOR shopContainer
@@ -52,70 +15,81 @@ const bagCustom = document.getElementById("bagCustom");
 //CAPTURO EL CONTENEDOR bagContainer
 const bagContainer = document.getElementById("bagContainer");
 
+//CAPTURO EL CONTENEDOR DEL LOGIN
+const loginContainer = document.getElementById("loginContainer");
+
+
 // EN ESTE ARRAY VACIO SE AGREGAN LOS PRODUCTOS QUE SE VAYAN COMPRANDO
 
 let carritoCustom = JSON.parse(localStorage.getItem("bagCustom")) || [];
 
-// RECORRO EL ARRAY Y AGREGO LOS ELEMENTOS dentro del  DIV
+//
+const getRemeras = async () => {
+    const respuestaRemeras = await fetch("remeras.json");
+    const dataRemeras = await respuestaRemeras.json();
 
-remerasCustom.forEach(remeras => {
-    let contenedor = document.createElement("div");
-    contenedor.className = "card"
-    contenedor.innerHTML = `
+    // RECORRO EL ARRAY Y AGREGO LOS ELEMENTOS dentro del  DIV
+
+    dataRemeras.forEach(remeras => {
+        let contenedor = document.createElement("div");
+        contenedor.className = "card"
+        contenedor.innerHTML = `
     <img class= "img" src="${remeras.img}">
     <h3 class="remeras">${remeras.nombre}<h3>
     <p class="talle">${remeras.talle}</p>
     <h2 class="precio">${remeras.precio} $</h2>
     `;
 
-    //AGREGO LA ESTRUCTURA HTML AL CONTENEDOR shopContainer
+        //AGREGO LA ESTRUCTURA HTML AL CONTENEDOR shopContainer
 
-    shopContainer.append(contenedor);
+        shopContainer.append(contenedor);
 
-    // CREO UN BOTON Y LO AGREGO AL CONTENEDOR shopContainer
+        // CREO UN BOTON Y LO AGREGO AL CONTENEDOR shopContainer
 
-    let comprar = document.createElement("button");
+        let comprar = document.createElement("button");
 
-    comprar.className = "comprar";
-    comprar.innerText = "comprar";
+        comprar.className = "comprar";
+        comprar.innerText = "comprar";
 
-    contenedor.append(comprar);
+        contenedor.append(comprar);
 
-    //el boton comprar permite agregar a la bolsa las remeras
+        //el boton comprar permite agregar a la bolsa las remeras
 
-    comprar.addEventListener("click", () => {
+        comprar.addEventListener("click", () => {
 
-        const repetida = carritoCustom.some((remeraRepetida) => remeraRepetida.id === remeras.id);
+            const repetida = carritoCustom.some((remeraRepetida) => remeraRepetida.id === remeras.id);
 
-        console.log(repetida);
+            console.log(repetida);
 
-        if (repetida) {
-            carritoCustom.map((rem) => {
-                if (rem.id === remeras.id) {
-                    rem.cantidad++;
-                }
-            });
-        } else {
-            carritoCustom.push({
-                id: remeras.id,
-                nombre: remeras.nombre,
-                img: remeras.img,
-                talle: remeras.talle,
-                cantidad: remeras.cantidad,
-                precio: remeras.precio,
-            });
-            //AGREGO LOS ELEMENTOS AL LOCAL STORAGE
-            bagLocal();
-        }
+            if (repetida) {
+                carritoCustom.map((rem) => {
+                    if (rem.id === remeras.id) {
+                        rem.cantidad++;
+                    }
+                });
+            } else {
+                carritoCustom.push({
+                    id: remeras.id,
+                    nombre: remeras.nombre,
+                    img: remeras.img,
+                    talle: remeras.talle,
+                    cantidad: remeras.cantidad,
+                    precio: remeras.precio,
+                });
+                //AGREGO LOS ELEMENTOS AL LOCAL STORAGE
+                bagLocal();
+            }
 
-        Swal.fire(
-            'buenismo!',
-            'tu producot esta en el carrito!',
-            'success'
-        )
+            Swal.fire(
+                'buenismo!',
+                'tu producto esta en el carrito!',
+                'success'
+            )
+        });
     });
-});
+};
 
+getRemeras();
 
 // la funcion flecha crea las estructura con un div con un titulo y un boton que oculta 
 //el contenedor, ademas recorre el carrito q contiene los productos selecionados y calcula el precio total.
@@ -124,11 +98,10 @@ const carritoBolsa = () => {
     //cada vez q se haga click en el icono bolsita se limpia el contenedor
     bagContainer.innerHTML = ""
     //muestro el icono bolsita
-    bagContainer.style.display = "grid";
 
     const bagHeader = document.createElement("div");
     bagHeader.className = "bag-header";
-    bagHeader.innerHTML = ` <h1 class ="bag-header-title">CUSTOM-BAG<h1>`;
+    bagHeader.innerHTML = `<h1 class ="bag-header-title">CUSTOM-BAG</h1>`;
 
     bagContainer.append(bagHeader);
 
@@ -147,10 +120,10 @@ const carritoBolsa = () => {
         bagCarrito.className = "bag-carrito";
         bagCarrito.innerHTML = `
     <img class = "bag-img" src="${remeras.img}">
-    <h3>Remera :${remeras.nombre}<h3>
+    <h3>Remera :${remeras.nombre}</h3>
     <p>Talle: ${remeras.talle}</p>
     <p>cantidad: ${remeras.cantidad}</p> 
-    <h2>Precio: ${remeras.precio}$</h2>
+    <h3>Precio: ${remeras.precio}$</h3>
     <p>subtotal: ${remeras.cantidad * remeras.precio}</p>
     <spam class ="bag-eliminar"> ❌</spam>
    
@@ -174,7 +147,7 @@ const carritoBolsa = () => {
 
     bagContainer.append(bagTotal);
 }
-//funcion flecha para eliminar los productos cuando se hace click en el icono "❌"
+//funcion flecha para eliminar los productos cuando se hace click en el icono 
 
 const eliminarRemera = (id) => {
 
@@ -199,11 +172,88 @@ const bagLocal = () => {
 };
 
 
+//LOGIN 
+//CREO LA ESTRUCTURA DEL FORM DONDE VOPY A PEDIR EL USUARIO Y SU CLAVE
 
-//LOGIN
+let formLogin = document.createElement("form")
+formLogin.id = "formLogin";
+formLogin.innerHTML = `
+ <input type="text" name="username" id="username-field" class="login-form-field" placeholder="Username">
+<input type="password" name="password" id="password-field" class="login-form-field" placeholder="Password">
+<input type="submit" value="Login" id="login-form-submit">
+<p id="mensajeContainer"><p/>
+`;
+loginContainer.append(formLogin);
 
-sesion.addEventListener("click", () => { 
-    if (document.getElementById("username").value == "usuario") {
-document.getElementById("mensaje").innerHTML=` <h2">acceso correcto <h2>`;
+//CAPTURO LO QUE INGRESA EL USUARIO EN LOS INPUTS 
+
+const loginForm = document.getElementById("formLogin");
+const loginButton = document.getElementById("login-form-submit");
+
+//COMPARO MEDIANTE UNA FUNCION LO QUE INGRESA EL USUARIO SI 
+//LA CONDICION ES VERDADERA
+loginButton.addEventListener("click", (e) => {
+    e.preventDefault();
+    const username = formLogin.username.value;
+    const password = formLogin.password.value;
+
+    if (username === "user" && password === "pass") {
+
+        //MUESTRO EL SIGUIENTE MENSAJE
+
+        const mensajeContainer = document.getElementById("mensajeContainer");
+
+
+        mensajeContainer.innerHTML = `sus datos son correctos`;
+        mensajeContainer.append(mensajeContainer);
+        location.reload();
+    } else {
+        // SI ES FALSO MUESTRO ESTE MENSAJE
+        mensajeContainer.innerHTML = `sus datos son incorrectos, por favor ingrese de nuevo`;
+        mensajeContainer.append(mensajeContainer);
+        location.reload();
+
     }
-})
+});
+
+
+//FOOTER
+
+//CAPTURO EL CONTENEDOR PARA PODER CREAR LA ESTRUCTURA HTML DEL FOOTER
+const footerContainer = document.getElementById("footerContainer")
+
+//CREO LA ESTRUCTURA HTML QUE VA A CONTENER EL LOGO, DOS PARRAFOS Y LAS REDES SOCIALES
+let footerGrupouno = document.createElement("div");
+footerGrupouno.className = "grupo-uno"
+footerGrupouno.innerHTML = `
+    <div class="box">
+        <figure>
+            <a href="#">
+                <img src="/imagenes/logo.png" alt="logo de la empresa">
+            </a>
+        </figure>
+    </div>
+    <div class="box">
+        <h2>SOBRE NOSOTROS</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus quo, optio ipsam saepe vitae</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus quo, optio ipsam saepe vitae</p>
+    </div>
+    <div class="box">
+        <h2>SIGUENOS</h2>
+        <div class="red-social">
+            <a href="#"><i class="bi bi-facebook"></i></a>
+            <a href="#"><i class="bi bi-instagram"></i></a>
+            <a href="#"><i class="bi bi-twitter-x"></i></a>
+            <a href="#"><i class="bi bi-threads"></i></a>
+        </div>
+    </div>
+`;
+//AGREGO LA ESTRUCTURA HTML AL CONTENEDOR
+footerContainer.append(footerGrupouno);
+
+//CREO LA ESTRUCTURA HTML 
+let footerGrupodos = document.createElement("div")
+footerGrupodos.className = "grupo-dos"
+footerGrupodos.innerHTML = `<small> <i class="bi bi-c-circle"></i><b> TODOS LOS DERECHOS RESERVADOS<b/></small>`;
+//AGREGO LA ESTRUCTURA HTML AL CONTENEDOR
+footerContainer.append(footerGrupodos);
